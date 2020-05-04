b10364929d5d9947a4e03351cff8de289a2feaa9
chad svastisalee <swasgrafx@gmail.com>
2020-05-04 11:47:32 -0500

    Lifting State Up

--- src/index.js
+++ src/index.js
@@ -5,28 +5,46 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
 
   render() {
     return (
       <button
-      className="square"
-      onClick={() => this.setState({value: 'X'})}
-    >
-      {this.state.value}
-    </button>
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
+    );
   }
 
   render() {
